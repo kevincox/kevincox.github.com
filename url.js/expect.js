3f914b75e4d1154252fe4dc0191a00bfc83f1a49
Kevin Cox <kevincox.ca@gmail.com>
2013-04-04 18:42:37 -0400

    URL.js test, take two.

---
+++ url.js/expect.js
@@ -0,0 +1,11 @@
+--2013-04-04 18:39:30--  https://raw.github.com/LearnBoost/expect.js/0.2.0/expect.js
+Resolving raw.github.com (raw.github.com)... 199.27.74.133
+Connecting to raw.github.com (raw.github.com)|199.27.74.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 35517 (35K) [text/plain]
+Saving to: ‘expect.js’
+
+     0K .......... .......... .......... ....                 100%  359K=0.1s
+
+2013-04-04 18:39:31 (359 KB/s) - ‘expect.js’ saved [35517/35517]
+
---
+++ url.js/mocha.js
@@ -0,0 +1,13 @@
+--2013-04-04 18:38:42--  https://raw.github.com/visionmedia/mocha/1.9.0/mocha.js
+Resolving raw.github.com (raw.github.com)... 199.27.73.133
+Connecting to raw.github.com (raw.github.com)|199.27.73.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 110771 (108K) [text/plain]
+Saving to: ‘mocha.js’
+
+     0K .......... .......... .......... .......... .......... 46%  591K 0s
+    50K .......... .......... .......... .......... .......... 92% 1.04M 0s
+   100K ........                                              100% 1.68M=0.1s
+
+2013-04-04 18:38:43 (794 KB/s) - ‘mocha.js’ saved [110771/110771]
+
